c54851f6366d7065025254722b806fa4eb1cde05
Swapnil-Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-10-27 12:37:02 +0530

    changes manifest and tried service worker

--- src/App.js
+++ src/App.js
@@ -431,8 +431,8 @@ function App() {
         </div>
 
         <div style={{ width: "inherit" }}>
-          <p class="game-explanation">
-            <strong class="important">How to play:</strong> Use your{" "}
+          <p className="game-explanation">
+            <strong className="important">How to play:</strong> Use your{" "}
             <strong>arrow keys</strong> to move the tiles. When two tiles with
             the same number touch, they <strong>merge into one!</strong>
           </p>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+window.addEventListener("load", () => {
+  registerSW();
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+async function registerSW() {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("./serviceWorker.js");
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
+}
+
+serviceWorker.register();
